f2fc5a7bca7738a67aeebae19196f6b8650f6d17
Matt <oystermenator@gmail.com>
2021-09-08 00:12:17 +0100

    Reset button added

--- scripts.js
+++ scripts.js
@@ -3,12 +3,24 @@
 for (let i = 0; i < (16 * 16); i++) {
     var node = document.createElement("div");
     node.className = "cell";
-    node.id = `cell${i}`;
     document.getElementById("container").appendChild(node);
 }
 
 document.querySelectorAll('.cell').forEach(item => {
     item.addEventListener('mouseenter', event => {
-        document.getElementById(event.target.id).style.border = '5px dotted orange';
+        event.target.classList.remove('cell');
+        event.target.classList.add('shaded');;
     })
 })
+
+function resetGame() {
+    const shadedList = document.querySelectorAll('.shaded');
+    shadedList.forEach(function (singleCell) {
+        singleCell.classList.remove('shaded');
+        singleCell.classList.add('cell');
+    });
+
+}
+
+const button = document.getElementById("reset");
+button.addEventListener('click', resetGame);
